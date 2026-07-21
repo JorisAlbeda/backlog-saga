6476d842bbe3c7de3f9228fb4ddde8850352835a
Joris Albeda <joris@albeda.net>
2026-07-21 23:22:36 +0200

    Added favicon.

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
 useHead({
-  titleTemplate: (chunk) => (chunk ? `Backlog Saga - ${chunk}` : 'Backlog Saga')
+  titleTemplate: (chunk) => (chunk ? `Backlog Saga - ${chunk}` : 'Backlog Saga'),
+  link: [
+    { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+    { rel: 'icon', href: '/favicon.ico' },
+    { rel: 'apple-touch-icon', href: '/favicon.png' }
+  ]
 })
 </script>
 
